b874da3f316e18c6e45ae07ae5cb24b334eff3c6
Matt Corby <mcorby313@gmail.com>
2020-04-06 19:36:33 -0400

    Initial commit

--- app/src/main/java/com/example/drawerexperiment/MainActivity.java
+++ app/src/main/java/com/example/drawerexperiment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.drawerexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
